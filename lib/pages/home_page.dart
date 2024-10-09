f7b1ec0bc358aa262fe7aacbaef6c944534342d6
yasithabhanuka <yasithabhanukac@gmail.com>
2024-10-09 16:11:04 +0530

    Add the safeArea into the main.dart return and make padding in tasks list widget. make the _addTaskButton widget and add it into the homepage as a floatingActionButton

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  HomePage({super.key});
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -14,34 +14,54 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     _deviceWidth = MediaQuery.of(context).size.width;
     _deviceHeight = MediaQuery.of(context).size.height;
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        elevation: 5.0,
-        shadowColor: Colors.black,
-        toolbarHeight: _deviceHeight * 0.15,
-        centerTitle: true,
-        title: const Text(
-          'TaskMe',
-          style: TextStyle(fontSize: 30.0, color: Colors.white),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          elevation: 5.0,
+          shadowColor: Colors.black,
+          toolbarHeight: _deviceHeight * 0.15,
+          centerTitle: true,
+          title: const Text(
+            'TaskMe',
+            style: TextStyle(fontSize: 30.0, color: Colors.white),
+          ),
+        ),
+        body: _tasksList(),
+        floatingActionButton: Padding(
+          padding: EdgeInsets.only(right: _deviceWidth * 0.01),
+          child: _addTaskButton(),
         ),
       ),
-      body: _tasksList(),
     );
   }
 
   Widget _tasksList() {
-    return ListView.builder(
-      itemCount: 5,
-      itemBuilder: (context, index) {
-        return Card(
-          child: ListTile(
-            title: Text('Task $index'),
-            subtitle: Text(DateTime.now().toString()),
-            trailing: const Icon(color: Colors.red, Icons.check_box_outlined),
-          ),
-        );
-      },
+    return Padding(
+      padding: EdgeInsets.all(_deviceWidth * 0.03),
+      child: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text('Task $index'),
+              subtitle: Text(DateTime.now().toString()),
+              trailing: const Icon(color: Colors.red, Icons.check_box_outlined),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _addTaskButton() {
+    return FloatingActionButton(
+      backgroundColor: Colors.red,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(50.0),
+      ),
+      onPressed: () {},
+      child: const Icon(Icons.add, color: Colors.white),
     );
   }
 }
